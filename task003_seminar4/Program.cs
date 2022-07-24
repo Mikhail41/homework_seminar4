ab0f86cb889180f8d63a1a9af99cc6c7cfc59bd7
Mikhail <mikhail-zmyslin@yandex.ru>
2022-07-25 00:43:29 +0300

    добавил решение третьей задачи

---
+++ task003_seminar4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+Console.WriteLine("Эта программа задаёт массив из 8 элементов и выводит их на экран.");
+
+int[] fillArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 111);
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+int[] arr = new int[8];
+fillArr(arr);
+PrintArray(arr);
